e359bf7a3250bc8922dcedccbe5367b685b2c203
Tony.cup <li926893@gmail.com>
2017-09-20 20:58:59 +0800

    复写rsa生存算法

--- src/main/java/com/junliang/helloworld/util/RSAHelper.java
+++ src/main/java/com/junliang/helloworld/util/RSAHelper.java
@@ -11,6 +11,7 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.Date;
 
 public class RSAHelper {
@@ -61,7 +62,7 @@ public class RSAHelper {
      * @throws IOException
      * @throws NoSuchAlgorithmException
      */
-    public static void generateKey(String publicKeyFilename, String privateKeyFilename, String password) throws IOException, NoSuchAlgorithmException {
+    public static void generateByteKey(String publicKeyFilename, String privateKeyFilename, String password) throws IOException, NoSuchAlgorithmException {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
         SecureRandom secureRandom = new SecureRandom(password.getBytes());
         keyPairGenerator.initialize(1024, secureRandom);
@@ -76,6 +77,31 @@ public class RSAHelper {
         fos.close();
     }
 
+    public static void generateBase64Key(String publicKeyFilename, String privateKeyFilename, String password) throws IOException, NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        SecureRandom secureRandom = new SecureRandom(password.getBytes());
+        keyPairGenerator.initialize(1024, secureRandom);
+        KeyPair keyPair = keyPairGenerator.genKeyPair();
+        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+        String stringPubKey= Base64.getEncoder().encodeToString(publicKeyBytes);
+        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(publicKeyFilename));
+        osw.write(stringPubKey);
+        osw.close();
+        byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
+        String stringPriKey= Base64.getEncoder().encodeToString(privateKeyBytes);
+        osw = new OutputStreamWriter(new FileOutputStream(privateKeyFilename));
+        osw.write(stringPriKey);
+        osw.close();
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        //公钥文件路径
+        String publicKeyFilePath = "D:\\workspace\\public.key";
+        //私钥文件路径
+        String privateKeyFilePath = "D:\\workspace\\private.key";
+        generateBase64Key(publicKeyFilePath,privateKeyFilePath,"2121");
+
+    }
     //public static void main(String[] args) throws Exception {
     //    //公钥文件路径
     //    String publicKeyFilePath = "D:\\workspace\\public.key";
