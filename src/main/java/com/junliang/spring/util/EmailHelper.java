e67b7fb8e29fe794f856b08944447c374c8d7cab
Tony.cup <li926893@gmail.com>
2017-09-30 16:59:29 +0800

    修改pom jar 包冲突

--- src/main/java/com/junliang/spring/util/EmailHelper.java
+++ src/main/java/com/junliang/spring/util/EmailHelper.java
@@ -1,4 +1,14 @@
 package com.junliang.spring.util;
 
 public class EmailHelper {
+
+    //TODO 2017/9/26 初步想法
+    /**
+     *
+     * 支持自定义发件人昵称
+     * 支持扩展邮件Message
+     * 支持抄送／HTML／附件
+     * 支持异步发送
+     * 支持邮件模板
+     */
 }
