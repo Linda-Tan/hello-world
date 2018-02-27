1ef7ea42aa3536aebde99b6a5a8c71fbf967c55f
junliang.com <li926893@gmail.com>
2018-02-27 13:53:57 +0800

    add ${revision}

--- src/main/java/com/junliang/spring/interceptor/AuthInterceptor.java
+++ src/main/java/com/junliang/spring/interceptor/AuthInterceptor.java
@@ -11,11 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     //TODO 网关鉴定token是否有效。拦截器检验用户或服务是否可以调用当前服务。这里需要搭建鉴权中心；
+
     @Value("myProps.auth.token-header")
     private String tokenHeader;
 
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
         return super.preHandle(request, response, handler);
     }
 }
