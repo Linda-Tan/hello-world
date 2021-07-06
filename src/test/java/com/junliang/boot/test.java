85b80b1ecfbe9b567af781f69a089995cfeed557
LiJunliang <junliang.li@huilianyi.com>
2021-07-06 19:58:18 +0800

    commit

--- src/test/java/com/junliang/boot/test.java
+++ src/test/java/com/junliang/boot/test.java
@@ -1,5 +1,9 @@
 package com.junliang.boot;
 
+import java.util.concurrent.locks.Lock;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * TODO desc
  * @author junlinag.li
@@ -8,19 +12,44 @@ package com.junliang.boot;
 public class test {
 
 	public static void main(String[] args) {
-		String fileUrl = "https://e-archives-dev.oss-cn-shanghai.aliyuncs.com/1367036488438124546/IMG/9ad115c7-8755-4e96-adeb-bebf16e93bd0-a1ec08fa513d269742a02aab55fbb2fb4216d8d0.jpg?Expires=1625202897&OSSAccessKeyId=LTAI4GCc2n26BfFhe1kEixQw&Signature=9Ltp6pBNCscLtAlKyxx%2B2qYyIO4%3D";
-		System.out.println(fileUrl);
+		// String fileUrl = "https://e-archives-dev.oss-cn-shanghai.aliyuncs.com/1367036488438124546/IMG/9ad115c7-8755-4e96-adeb-bebf16e93bd0-a1ec08fa513d269742a02aab55fbb2fb4216d8d0.jpg?Expires=1625202897&OSSAccessKeyId=LTAI4GCc2n26BfFhe1kEixQw&Signature=9Ltp6pBNCscLtAlKyxx%2B2qYyIO4%3D";
+		// System.out.println(fileUrl);
+		//
+		// //移除参数
+		// int index = fileUrl.lastIndexOf("?");
+		// if (index != -1) {
+		// 	fileUrl = fileUrl.substring(0, index);
+		// }
+		// if (index != -1) {
+		// 	index = fileUrl.lastIndexOf("/");
+		// 	fileUrl = fileUrl.substring(index + 1);
+		// }
+		//
+		// System.out.println(fileUrl);
 
-		//移除参数
-		int index = fileUrl.lastIndexOf("?");
-		if (index != -1) {
-			fileUrl = fileUrl.substring(0, index);
-		}
-		if (index != -1) {
-			index = fileUrl.lastIndexOf("/");
-			fileUrl = fileUrl.substring(index + 1);
+		/*
+		 *   "fileInfoPath":[
+		 * "/[custFormValues]/[attachmentImages]/fileURL",
+		 * "/pdfFileURL"
+		 * ]
+		 */
+
+		String urlMappingPath = "/[custFormValues]/[attachmentImages]/fileURL";
+		String[] split = urlMappingPath.split("/");
+
+		// StringUtils.
+
+		System.out.println(split.length);
+		for (int i = 0; i < split.length; i++) {
+			System.out.println(split[i]);
+			String str =  split[i];
+			if ( str.startsWith("[")&&str.endsWith("]")){
+
+			}
+
+			if (i == split.length -1)
+				System.out.println(split[i]);
 		}
 
-		System.out.println(fileUrl);
 	}
 }
